756be512ba729abb648268392f8d60d2d4274ffb
Ronit Jain <rjra2611@gmail.com>
2022-04-20 15:11:38 +0530

    shift method to child classes

--- models/configuration.py
+++ models/configuration.py
@@ -2,12 +2,16 @@ import click
 import abc
 from lean.components.util.logger import Logger
 
-class Configuration():
+class Configuration(abc.ABC):
     def __init__(self, config_json_object):
         self._name = config_json_object["Name"]
         self._config_type = config_json_object["Type"]
         self._value = config_json_object["Value"]
         self._envrionment = config_json_object["Environment"]
+
+    @abc.abstractmethod
+    def is_required_from_user(self):
+        return NotImplemented()
 
     def factory(config_json_object):
         if config_json_object["Type"] == "info":
@@ -15,12 +19,12 @@ class Configuration():
         elif config_json_object["Type"] == "input":
             return UserInputConfiguration.factory(config_json_object)
 
-    def is_required_from_user(self):
-        return self._config_type == "input"
-
 class InfoConfiguration(Configuration):
     def __init__(self, config_json_object):
         super().__init__(config_json_object)
+
+    def is_required_from_user(self):
+        return False
 
 class UserInputConfiguration(Configuration, abc.ABC):
     def __init__(self, config_json_object):
@@ -29,6 +33,10 @@ class UserInputConfiguration(Configuration, abc.ABC):
         self._input_type = config_json_object["Input-type"]
         self._input_data = config_json_object["Input-data"]
         self._help = config_json_object["Help"]
+
+    @abc.abstractmethod
+    def AskUserForInput(self, default_value):
+        return NotImplemented()
 
     def factory(config_json_object):
         if config_json_object["Input-method"] == "prompt":
@@ -40,9 +48,8 @@ class UserInputConfiguration(Configuration, abc.ABC):
         elif config_json_object["Input-method"] == "prompt-password":
             return PromptPasswordUserInput(config_json_object)
 
-    @abc.abstractmethod
-    def AskUserForInput(self, default_value):
-        return NotImplemented()
+    def is_required_from_user(self):
+        return True
 
 class PromptUserInput(UserInputConfiguration):
     def __init__(self, config_json_object):
